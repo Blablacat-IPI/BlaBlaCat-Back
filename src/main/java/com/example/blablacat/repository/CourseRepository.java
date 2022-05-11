9c93097b9c51741d557dac51f6fe4d226e70bb06
audreylory <47520459+audreylory@users.noreply.github.com>
2022-05-11 13:05:11 +0200

    Recuperation des courses si deleted_at null et date > now

--- src/main/java/com/example/blablacat/repository/CourseRepository.java
+++ src/main/java/com/example/blablacat/repository/CourseRepository.java
@@ -5,11 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public interface CourseRepository extends JpaRepository<CourseEntity, Integer> {
 
     List<CourseEntity> findByCityDepartureOrCityArrival(String cityDeparture, String cityArrival);
+
+    List<CourseEntity> findAllByDeletedAtNullAndDateAfter(LocalDateTime dateMin);
 
 }
