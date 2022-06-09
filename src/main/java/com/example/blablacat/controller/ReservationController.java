17703ba0823a1f8bda13a2a192d371e118de309b
Loïck Jouan <ltevidence@MacBook-Pro-de-Loick.local>
2022-06-09 15:31:00 +0200

    changing localhost:4200 for Vercel app

--- src/main/java/com/example/blablacat/controller/ReservationController.java
+++ src/main/java/com/example/blablacat/controller/ReservationController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins="https://blablacat.vercel.app")
+@CrossOrigin(origins="https://front-v2-seven.vercel.app/")
 @RestController
 @RequestMapping("Reservation")
 public class ReservationController {
