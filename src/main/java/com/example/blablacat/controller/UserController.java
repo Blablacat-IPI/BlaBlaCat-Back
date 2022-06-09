544f6df261fa34163a3aecd816e5fe7fc11eee60
Loïck Jouan <ltevidence@MacBook-Pro-de-Loick.local>
2022-06-09 15:23:55 +0200

    changing localhost:4200 for Vercel app

--- src/main/java/com/example/blablacat/controller/UserController.java
+++ src/main/java/com/example/blablacat/controller/UserController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://blablacat.vercel.app")
 @RestController
 @RequestMapping("Users")
 public class UserController {
