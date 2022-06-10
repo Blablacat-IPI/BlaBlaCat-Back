cbc8ae17dbd06791847edbc76d237f529381c778
Loïck Jouan <ltevidence@MacBook-Pro-de-Loick.local>
2022-06-10 11:06:16 +0200

    change url cross origins

--- src/main/java/com/example/blablacat/controller/AuthentificationController.java
+++ src/main/java/com/example/blablacat/controller/AuthentificationController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin(origins = "https://blablacat.vercel.app/")
+@CrossOrigin(origins = "https://blablacat.vercel.app")
 @RequestMapping("Auth")
 public class AuthentificationController {
 
--- src/main/java/com/example/blablacat/controller/CourseController.java
+++ src/main/java/com/example/blablacat/controller/CourseController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "https://front-v2-seven.vercel.app/")
+@CrossOrigin(origins = "https://blablacat.vercel.app")
 @RestController
 @RequestMapping("Course")
 public class CourseController {
